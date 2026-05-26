63f80321110229a6509864a7efae612722a941e8
Saiswee <saiswee207@gmail.com>
2026-05-26 12:15:09 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -44,19 +44,16 @@ function App() {
       {/* ABOUT */}
       <section className="section">
   <h2>About Me</h2>
-
-  <p>
-    🎓 Third-Year Engineering Student<br />
-    💻 Python Developer | AI & ML Enthusiast | IoT Builder<br />
-    🚀 Passionate about building real-world intelligent systems
-  </p>
-
-  <ul>
-    <li>🔭 Working on AI, ML & IoT projects</li>
-    <li>🌱 Improving DSA, ML models & system design</li>
-    <li>🧠 Interested in smart automation & problem solving</li>
-    <li>🎯 Goal: AI/ML Engineer / Software Developer</li>
-  </ul>
+<p>
+  🎓 Third-Year Engineering Student<br />
+  💻 Python Developer | AI & ML Enthusiast | IoT Builder<br />
+  🚀 Passionate about building real-world intelligent systems<br />
+  <br />
+  - 🔭 Working on AI, ML & IoT projects<br />
+  - 🌱 Improving DSA, ML models & system design<br />
+  - 🧠 Interested in smart automation & problem solving<br />
+  - 🎯 Goal: AI/ML Engineer / Software Developer<br />
+</p>
 </section>
 
       {/* SKILLS */}
